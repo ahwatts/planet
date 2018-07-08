1f8873b97e590d0f8df1aab0d6f3de25e83119c3
Andrew Watts <ahwatts@gmail.com>
2018-07-08 19:23:13 -0400

    Don't get mad. Get glad!

--- src/planet.cpp
+++ src/planet.cpp
@@ -3,12 +3,14 @@
 #include <iostream>
 #include <string>
 
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <glm/vec3.hpp>
 
 void bailout(const std::string &msg);
 void handleGlfwError(int code, const char *desc);
+void initGlad();
 void initGlfw(int width, int height, const char *title, GLFWwindow **window);
 
 const int WINDOW_WIDTH = 1024, WINDOW_HEIGHT = 768;
@@ -17,6 +19,13 @@ const char *WINDOW_TITLE = "Planet Demo";
 int main(int argc, char **argv) {
     GLFWwindow *window;
     initGlfw(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, &window);
+    initGlad();
+
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+    std::cout << "OpenGL renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL vendor: " << glGetString(GL_VENDOR) << std::endl;
+
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
@@ -33,12 +42,23 @@ void handleGlfwError(int code, const char *desc) {
               << desc << std::endl;
 }
 
+void initGlad() {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        bailout("Could not initialize GLAD");
+    }
+}
+
 void initGlfw(int width, int height, const char *title, GLFWwindow **window) {
     glfwSetErrorCallback(handleGlfwError);
     if (!glfwInit()) {
         bailout("Could not initialize GLFW");
     }
 
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     *window = glfwCreateWindow(width, height, title, nullptr, nullptr);
 
     if (!*window) {
@@ -46,9 +66,4 @@ void initGlfw(int width, int height, const char *title, GLFWwindow **window) {
     }
 
     glfwMakeContextCurrent(*window);
-
-    // std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
-    // std::cout << "GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
-    // std::cout << "OpenGL renderer: " << glGetString(GL_RENDERER) << std::endl;
-    // std::cout << "OpenGL vendor: " << glGetString(GL_VENDOR) << std::endl;
 }
